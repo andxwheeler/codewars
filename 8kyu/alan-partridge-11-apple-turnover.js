1ad09fd608608424ba797ed2507167ecc89c2dde
Andrew Wheeler <andxwheeler@icloud.com>
2022-12-01 18:28:29 -0500

    Add description of the problem and the function apple(x) to the solution

---
+++ 8kyu/alan-partridge-11-apple-turnover.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Note: Input will either be a positive integer (or a string for untyped languages).
+
+// Solution:
+
+function apple(x) {
+  if (Math.pow(x,2) > 1000) {
+    return "it's hotter than the sun!!"
+  } else {
+    return "Help yourself to a honeycomb Yorkie from the glovebox."
+  }
+}
